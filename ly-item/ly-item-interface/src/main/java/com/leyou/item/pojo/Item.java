b8edc49a22a1659d5bd92f57b67b24ab9efea417
lijiqi04 <1336424471@qq.com>
2019-10-09 09:26:00 +0800

    leyou update 2019/10/9

--- ly-item/ly-item-interface/src/main/java/com/leyou/item/pojo/Item.java
+++ ly-item/ly-item-interface/src/main/java/com/leyou/item/pojo/Item.java
@@ -1,5 +1,34 @@
 package com.leyou.item.pojo;
 
-public class Item {
+import lombok.Data;
 
+@Data
+public class Item {
+    private Integer id;
+    private String name;
+    private Long price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
 }
--- ly-item/ly-item-service/src/main/java/com/leyou/LyItemService.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemService.java
@@ -1,4 +1,4 @@
-package com.li;
+package com.leyou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
